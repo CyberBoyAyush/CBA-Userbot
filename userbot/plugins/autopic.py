cb08fb4932411dc64b44a387aa7b42afb1d7f160
STARKGANG <StarkGangZ@gmail.com>
2020-05-30 15:52:23 +0530

    Update autopic.py

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -28,7 +28,7 @@ async def autopic(event):
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((95, 250), current_time, font=fnt, fill=(500, 500, 500))
+        drawn_text.text((500, 1000), current_time, font=fnt, fill=(50000, 50000, 50000))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
