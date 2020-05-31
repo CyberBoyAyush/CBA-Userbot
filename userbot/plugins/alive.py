2cfa73eff6135579481a1fa498a3d5a78f374a13
STARKGANG <StarkGangZ@gmail.com>
2020-05-31 17:52:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,12 +13,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`FRIDAY: ` **✅ Alive**\n\n"
-                     f"`My Boss`: {DEFAULTUSER}\n\n"
-                     "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **ALL NORMAL!WORKING FINE 🙂**\n\n`Always with you, my Boss!!\n`"
-                     "**Satellite Name:** 🛰️HEROKUSAT-2🛰️\n"
-                     "**Satellite Signal Strength:** 92 Db\n"
-                     "**Satellite Staus:** ✅ Alive\n\n"
-                     "     [❤️Deploy FRIDAY❤️](https://github.com/StarkGang/FRIDAYUserbot)") 
+    await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
+                     "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+                     "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 🙂**\n\n`NO PROBLEM FOUND🔥!\n`"
+                     "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
+                     "`CURRECT BOT LOCATION:` ** ASHBURN,UNITED STATES\n"
+                     "`DEPLOYING SERVICE BY: HEROKU INC.\n"
+                     "`IP:` **3.82.66.232\n"
+                     "`SYSTEM TYPE:` **LINUX\n"
+                     "`BASED ON:` **UNIBORG\n"
+                     "`SATELLITE:` **⚡STARK SAT-2⚡\n"
+                     "`MADE USING:` **PYTHON\n"
+                     f"`MY BOSS`: {DEFAULTUSER}\n\n"
+                     "ALWAYS WITH YOU MY BOSS"
+                     "`SATELLITE STATUS:** ✅ Alive\n\n"
+                     "[⚡DEPLOY FRIDAY⚡](https://github.com/StarkGang/FRIDAYUserbot)") 
 
