b85c0561aca0f6e41d09451241399a816fdd1ac1
STARKGANG <StarkGangZ@gmail.com>
2020-05-31 10:36:11 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -4,27 +4,23 @@
 # you may not use this file except in compliance with the License.
 #
 
-import asyncio
 from asyncio import wait
 
+from telethon import events
 
-from userbot.events import register
 
-@register(outgoing=True, pattern="^.tspam")
-async def tmeme(e):
-    tspam = str(e.text[7:])
-    message = tspam.replace(" ", "")
-    for letter in message:
-        await e.respond(letter)
-    await e.delete()
 
-@register(outgoing=True, pattern="^.spam")
+@borg.on(events.NewMessage(pattern=r"\.spam", outgoing=True))
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
         counter = int(message[6:8])
         spam_message = str(e.text[8:])
-        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+
+        await wait(
+            [e.respond(spam_message) for i in range(counter)]
+            )
+
         await e.delete()
         if LOGGER:
             await e.client.send_message(
@@ -32,50 +28,3 @@ async def spammer(e):
                 "#SPAM \n\n"
                 "Spam was executed successfully"
                 )
-                               
-@register(outgoing=True, pattern="^.bigspam")
-async def bigspam(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        counter = int(message[9:13])
-        spam_message = str(e.text[13:])
-        for i in range(1, counter):
-            await e.respond(spam_message)
-        await e.delete()
-        if LOGGER:
-            await e.client.send_message(
-                LOGGER_GROUP,
-                "#BIGSPAM \n\n"
-                "Bigspam was executed successfully"
-                )
-        
-        
-@register(outgoing=True, pattern="^.picspam")
-async def tiny_pic_spam(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        text = message.split()
-        counter = int(text[1])
-        link = str(text[2])
-        for i in range(1, counter):
-            await e.client.send_file(e.chat_id, link)
-        await e.delete()
-        if LOGGER:
-            await e.client.send_message(
-                LOGGER_GROUP,
-                "#PICSPAM \n\n"
-                "PicSpam was executed successfully"
-                )
-@register(outgoing=True, pattern="^.delayspam (.*)")
-async def spammer(e):
-    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
-    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
-    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
-    await e.delete()
-    for i in range(1, counter):
-        await e.respond(spam_message)
-        await sleep(spamDelay)
-    if LOGGER:
-        await e.client.send_message(
-            LOGGER_GROUP, "#DelaySPAM\n"
-            "DelaySpam was executed successfully")
