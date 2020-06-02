80061c69f74a3475aac2a92bddbe963bfa5af843
STARKGANG <StarkGangZ@gmail.com>
2020-06-02 10:55:28 +0530

    Fixed A Bug

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -61,7 +61,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "@IndianArMyGiveaway.png"
+            remove_bg_image.name = "@FridayOT.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
