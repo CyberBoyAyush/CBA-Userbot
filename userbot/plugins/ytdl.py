dbca543dbf41e3972b62aaee8181e34ae1efe710
STARKGANG <StarkGangZ@gmail.com>
2020-06-02 10:42:56 +0530

    Added 480p Quality As Request From @JosueGhost12

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -103,7 +103,7 @@ async def download_video(v_url):
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': '320',
+                'preferredquality': '480',
             }],
             'outtmpl':
             '%(id)s.mp3',
