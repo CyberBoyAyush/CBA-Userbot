07b8711cc2c80304c0f700f282d932b0371a3b45
STARKGANG <StarkGangZ@gmail.com>
2020-06-03 14:47:07 +0530

    Update animinationz.py

--- userbot/plugins/animinationz.py
+++ userbot/plugins/animinationz.py
@@ -105,7 +105,7 @@ async def _(event):
     await event.delete() 
     
     
-@borg.on(admin_cmd("sexygirl"))
+@borg.on(admin_cmd("ohh"))
 async def _(event):
     if event.fwd_from:
         return
