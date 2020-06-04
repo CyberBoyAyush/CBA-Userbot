6bead2ff90d1e4f424727547edc8cdf4a18959ad
STARKGANG <StarkGangZ@gmail.com>
2020-06-04 12:18:00 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -41,7 +41,7 @@ async def apk(e):
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n===> @Bot_Hub_Official <==="
+        app_details += "\n\n===> @FridayOT <==="
         await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
         await e.edit("No result found in search. Please enter **Valid app name**")
