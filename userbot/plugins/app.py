12275c2fd132359cfd373fae92cbf9a0e900a015
STARKGANG <StarkGangZ@gmail.com>
2020-06-04 11:56:15 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -56,13 +56,11 @@ async def apk(e):
 
         app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
 
-        app_details = "<a href='"+app_icon+"'>ð²&#8203;</a>"
+        app_details = "<a href='"+app_icon+"'>ЁЯУ▓&#8203;</a>"
 
         app_details += " <b>"+app_name+"</b>"
 
-        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
-
-        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "â­ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "â­ ").replace("five", "5")
+        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>
 
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
 
