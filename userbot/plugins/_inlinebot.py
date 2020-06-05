3b0fc8e3092bfcff4f71c918606f9eea02bd2b80
STARKGANG <StarkGangZ@gmail.com>
2020-06-05 09:02:45 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -17,7 +17,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© FRIDAYUserbot Help",
+                "© FRIDAYUserBot Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use another's\n ib @mariodevs for learning how to get userbot!"
+            reply_pop_up_alert = "Get your own userbot, don't use others\n ib @mariodevs for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
