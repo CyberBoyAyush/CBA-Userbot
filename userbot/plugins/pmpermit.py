10f2debba6b386878168390bc9e1ecd3eb3d321f
STARKGANG <StarkGangZ@gmail.com>
2020-06-06 12:43:54 +0530

    New ☣️

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -13,7 +13,7 @@ PREV_REPLY_MESSAGE = {}
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
-USER_BOT_NO_WARN = ("  ╭━━━━━━━╮ \n┃     ● ══      ┃ \n┃███████┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃\n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃███████┃ \n┃        O       ┃\n╰━━━━━━━╯\n\n"
+USER_BOT_NO_WARN = ("  ╭━━━━━━━╮ \n┃     ● ══      ┃ \n┃███████┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃\n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃██  ⚠️  ██┃ \n┃███████┃ \n┃        O        ┃\n╰━━━━━━━╯\n\n"
                     "`Hello ! This is` **F.R.I.D.A.Y**\n"
                     "`Private Messaging Security Protocol ⚠️`\n\n"
                     "**You Have Trespassed To My Masters"
