9ca0fc600f06acbfba80f0096941078ea1952680
STARKGANG <StarkGangZ@gmail.com>
2020-06-07 08:49:26 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -115,19 +115,3 @@ def paginate_help(page_number, loaded_plugins, prefix):
              custom.Button.inline("Next⏩", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
-
-@borg.on(admin_cmd(pattern="syntax (.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    plugin_name = event.pattern_match.group(1)
-    if plugin_name in borg._plugins:
-        help_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @UniBorg"
-        if help_string:
-            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
-        else:
-            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
-    else:
-        plugin_syntax = "Enter valid **Plugin** name.\nDo `.exec ls stdplugins` or `.helpme` to get list of valid plugin names."
-    await event.edit(plugin_syntax)
