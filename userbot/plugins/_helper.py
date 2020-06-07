463c456f9eb49bdda205a2b8cedebace9630ca28
STARKGANG <StarkGangZ@gmail.com>
2020-06-07 08:16:31 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -51,19 +51,3 @@ async def cmd_list(event):
             )
             await event.delete()
 
-@borg.on(admin_cmd(pattern="syntax (.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    plugin_name = event.pattern_match.group(1)
-    if plugin_name in borg._plugins:
-        help_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © FRIDAYUserbot"
-        if help_string:
-            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
-        else:
-            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
-    else:
-        plugin_syntax = "Enter valid **Plugin** name.\nDo `.exec ls plugins` or `.help` to get list of valid plugin names."
-    await event.edit(plugin_syntax)
-
