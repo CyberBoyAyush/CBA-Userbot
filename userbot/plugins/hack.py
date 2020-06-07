daf5b1d58fc5a88c30fcd467c3c76a6b54ff7c31
STARKGANG <StarkGangZ@gmail.com>
2020-06-07 12:32:32 +0530

    Lol 😂

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -33,7 +33,7 @@ async def _(event):
         usname = replied_user.user.username
         idd = reply_message.from_id
         if idd==813878981:
-            await event.edit("This is My Master\nI can't hack my master's Account\n**How dare you trying to hack my master's account AssKisser!**\n\n__Your account has been hacked! Pay 69$ to my master__ [Indian Bhai](tg://user?id=953414679) __to release your account__😏")
+            await event.edit("This is My Master\nI can't hack my master's Account\n**How dare you trying to hack my master's account AssKisser!**\n\n__Your account has been hacked! Pay 69$ to my master__ [FridayOT](t.me/fridayOT) __to release your account__😏")
         else:
             await event.edit("Hacking..")
             animation_chars = [
