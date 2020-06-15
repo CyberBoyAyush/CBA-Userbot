7cd6f48d69c2f2383de19c3263080d6faa5c8f04
STARKGANG <StarkGangZ@gmail.com>
2020-06-15 21:38:15 +0530

    Update proxyandbinchecker.py

--- userbot/plugins/proxyandbinchecker.py
+++ userbot/plugins/proxyandbinchecker.py
@@ -25,7 +25,7 @@ async def _(event):
               await conv.send_message("/start")
               await event.delete()
               response = await conv.get_response()
-              await conv.send_message("!proxy " + sysarg)
+              await conv.send_message("!proxy")
               audio = await conv.get_response()
               await borg.send_message(event.chat_id, audio.text)
           except YouBlockedUserError:
@@ -36,7 +36,7 @@ async def _(event):
               await conv.send_message("/start")
               await event.delete()
               response = await conv.get_response()
-              await conv.send_message("!proxy ")
+              await conv.send_message("!proxy " + sysarg)
               audio = await conv.get_response()
               await borg.send_message(event.chat_id, audio.text)
           except YouBlockedUserError:
