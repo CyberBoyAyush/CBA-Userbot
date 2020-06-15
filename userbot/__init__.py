14a9f71ceebefbf41e13bdd90c37d028eef99964
STARKGANG <StarkGangZ@gmail.com>
2020-06-15 21:19:29 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -125,7 +125,7 @@ if bool(ENV):
     # Clean Welcome
     CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
     # Custom Module
-CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
+    CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
     CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
     CUSTOM_ANIMATED_PACK_NAME = os.environ.get("CUSTOM_ANIMATED_PACK_NAME", None)
 
