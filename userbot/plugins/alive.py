f1f40913c9b1ab5d5a44a5070ac8e060da7f7966
STARKGANG <StarkGangZ@gmail.com>
2020-06-20 21:08:10 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,13 +13,12 @@ FRIDAY_IS_ALIVE = ("**☘︎F.R.I.D.A.Y☘︎** IS AT YOUR SERVICE❗\n\n"
                    "**♧︎︎︎Current Branch♧︎︎︎** : `master`\n\n"
                    "**♧︎︎︎Python Version♧︎︎︎** : `3.8`\n\n"
                    "**♧︎︎︎Friday OS♧︎︎︎** : `3.0 Snapdragon`\n\n"
-                   "**♧︎︎︎Current Sat♧︎︎︎ : `StarkGangSat-2.0`\n\n"
+                   "**♧︎︎︎Current Sat♧︎︎︎** : `StarkGangSat-2.0`\n\n"
                    f"**♧︎︎︎My Boss♧︎︎︎** : {DEFAULTUSER} \n\n"
-                   "**♧︎︎︎Updates♧︎︎︎** : `Found`\n\n"
                    "**♧︎︎︎Heroku Database♧︎︎︎** : `No Known Error Found`\n\n"
                    "**♧︎︎︎License♧︎︎︎** : [MIT Licence](Github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
                    "♧︎︎︎Copyright♧︎︎︎ : By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
-                   " [⌨︎♧︎︎︎Deploy Friday Userbot♧︎︎︎⌨︎](https://telegra.ph/FRIDAY-06-15)") 
+                   " [⌨︎Deploy FridayUserbot⌨︎](https://telegra.ph/FRIDAY-06-15)") 
 
 
 #@command(outgoing=True, pattern="^.alive$")
