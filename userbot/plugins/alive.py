367cefadffa24649e553d2363295d24db338e67b
STARKGANG <StarkGangZ@gmail.com>
2020-06-21 11:44:31 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,7 +8,8 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-FRIDAY_IS_ALIVE = ("**☘︎F.R.I.D.A.Y☘︎** IS AT YOUR SERVICE❗\n\n"
+FRIDAY_IS_ALIVE = ("**F.R.I.D.A.Y** IS AT YOUR SERVICE❗\n\n"
+                   "**I am Always Alive To Help You Boss**\n\n"
                    "**♧︎︎︎Currently Status♧︎︎︎** : `No Issue Found`\n\n"
                    "**♧︎︎︎Current Branch♧︎︎︎** : `master`\n\n"
                    "**♧︎︎︎Python Version♧︎︎︎** : `3.8`\n\n"
@@ -16,7 +17,7 @@ FRIDAY_IS_ALIVE = ("**☘︎F.R.I.D.A.Y☘︎** IS AT YOUR SERVICE❗\n\n"
                    "**♧︎︎︎Current Sat♧︎︎︎** : `StarkGangSat-2.0`\n\n"
                    f"**♧︎︎︎My Boss♧︎︎︎** : {DEFAULTUSER} \n\n"
                    "**♧︎︎︎Heroku Database♧︎︎︎** : `No Known Error Found`\n\n"
-                   "**♧︎︎︎License♧︎︎︎** : [MIT Licence](Github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
+                   "**♧︎︎︎License♧︎︎︎** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
                    "♧︎︎︎Copyright♧︎︎︎ : By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
                    " [⌨︎Deploy FridayUserbot⌨︎](https://telegra.ph/FRIDAY-06-15)") 
 
