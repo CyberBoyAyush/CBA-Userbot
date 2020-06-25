3c80162fde45f9830f2a555bfe1c0c955109b959
STARKGANG <StarkGangZ@gmail.com>
2020-06-25 17:01:53 +0530

    Test

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -6,7 +6,7 @@ from telethon.tl import functions
 import asyncio
 import shutil
 
-FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSerifCondensed-Bold.ttf"
 
 @command(pattern="^.autopic", outgoing=True)
 async def autopic(event):
@@ -25,7 +25,7 @@ async def autopic(event):
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((150, 350), current_time, font=fnt, fill=(255, 255, 255))
+        drawn_text.text((95, 250), current_time, font=fnt, fill=(500, 500, 500))
         img.save(photo)
         file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
