2a74b1fd4543250c8ef5f97c88080d8668425fc4
StarkGang <StarkGangZ@gmail.com>
2020-06-29 15:34:57 +0530

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -73,7 +73,7 @@ HIT = [
     "bashes",
 ]
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "IndianBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FridayUserbot"
 
 @borg.on(admin_cmd(pattern="slap ?(.*)", allow_sudo=True))
 async def who(event):
