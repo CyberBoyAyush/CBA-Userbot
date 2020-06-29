db91b73435f0e16b4336cd5e62c9e0b3f135e771
StarkGang <StarkGangZ@gmail.com>
2020-06-29 15:43:04 +0530

    Fix

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -8,7 +8,7 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "IndianBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Friday Userbot"
 
 @borg.on(admin_cmd(pattern=r"police"))
 
