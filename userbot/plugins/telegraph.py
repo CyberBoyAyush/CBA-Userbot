79f03e62275f96ca03301309635750d4ead01ba1
StarkGang <StarkGangZ@gmail.com>
2020-06-29 15:33:25 +0530

    Fixed

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -1,7 +1,7 @@
-"""@telegraph Utilities updated for Lazy people by @WhySooSerious 
+"""@telegraph Plugin For Lazy People
 Available Commands:
-.tgm reply to a media
-.tgt as reply to a large text"""
+.telegraph media as reply to a media
+.telegraph text as reply to a large text"""
 from telethon import events
 import os
 from PIL import Image
@@ -14,7 +14,7 @@ r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
-@borg.on(admin_cmd("t(m|t) ?(.*)"))
+@borg.on(admin_cmd("telegraph (media|text) ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -32,7 +32,7 @@ async def _(event):
         start = datetime.now()
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
-        if input_str == "m":
+        if input_str == "media":
             downloaded_file_name = await borg.download_media(
                 r_message,
                 Config.TMP_DOWNLOAD_DIRECTORY
@@ -53,7 +53,7 @@ async def _(event):
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
                 await event.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
-        elif input_str == "t":
+        elif input_str == "text":
             user_object = await borg.get_entity(r_message.from_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
