e3c3fed81f67ebeed3f9d4f2daa690530df0e49b
StarkGang <StarkGangZ@gmail.com>
2020-07-01 19:37:14 +0530

    Update lastupdate.py

--- userbot/plugins/lastupdate.py
+++ userbot/plugins/lastupdate.py
@@ -9,9 +9,42 @@ import time
 
 
 
-@borg.on(admin_cmd(pattern="lastupdate"))
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+#@command(pattern="^.updateinfo")
+@borg.on(admin_cmd(pattern="updateinfo"))
 async def _(event):
     if event.fwd_from:
         return
-    updatedtime = get_readable_time((time.time() - Lastupdate))
-    await event.edit(f"🔼Last Updated or Restarted!\nOn : {updatedtime}")
+    start = datetime.now()
+    await event.edit("Calculating Last Update or Restart Time")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    uptime = get_readable_time((time.time() - Lastupdate))
+    await event.edit(f"🔰 Friday Userbot Has Been Restarted Or Updated {uptime} Ago !")
