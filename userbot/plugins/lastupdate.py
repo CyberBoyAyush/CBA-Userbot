83658feaf2b0efa4949cca6f9cf479957efb3343
StarkGang <StarkGangZ@gmail.com>
2020-07-01 17:19:17 +0530

    Create lastupdate.py

---
+++ userbot/plugins/lastupdate.py
@@ -0,0 +1,19 @@
+from telethon import events
+from datetime import datetime
+from telethon import events
+from datetime import datetime
+from userbot.utils import admin_cmd
+from userbot.__init__ import Lastupdate
+import time
+
+
+
+@borg.on(admin_cmd(pattern="lastupdate"))
+async def _(event):
+    if event.fwd_from:
+        return
+        start = datetime.now()
+    await event.edit("🔼 Last Updated or Restarted!")
+    end = datetime.now()
+    updatedtime = get_readable_time((time.time() - Lastupdate))
+    await event.edit(f"🔼 Last Updated! or Restarted!\nOn : {updatedtime}")
