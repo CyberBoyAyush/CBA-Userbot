58e45f1f1ea92c330dc0b0f8a71f1c69f729b285
StarkGang <StarkGangZ@gmail.com>
2020-07-01 13:52:11 +0530

    Update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -58,9 +58,9 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by @IndianArMyGiveaway".format(ms))
+        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by Friday Userbot".format(ms))
     else:
-        await event.edit("ReMove.BG API returned Errors. Please report to @IndianArMyGiveaway\n`{}".format(output_file_name.content.decode("UTF-8")))
+        await event.edit("ReMove.BG API returned Errors. Please report to @FridayOt Support Group\n`{}".format(output_file_name.content.decode("UTF-8")))
 
 
 # this method will call the API, and return in the appropriate format
