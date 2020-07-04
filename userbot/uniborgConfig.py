26dde537e01c731155558b766894eab598c436d1
StarkGang <StarkGangZ@gmail.com>
2020-07-04 17:20:18 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -78,12 +78,12 @@ if ENV:
         # specify list of users allowed to use bot
         # WARNING: be careful who you grant access to your bot.
         # malicious users could do ".exec rm -rf /*"
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1097131648").split())
-        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "832241419").split())
+        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
         BLACKLIST_USERS = set(int(x) for x in os.environ.get("BLACKLIST_USERS", "").split())
-        DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "813878981").split())
-        OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "953414679").split())
-        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "719195224").split())
+        DEVLOPERS = set(int(x) for x in os.environ.get("DEVLOPERS", "").split())
+        OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
+        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
         # Very Stream
         VERY_STREAM_LOGIN = os.environ.get("VERY_STREAM_LOGIN", None)
         VERY_STREAM_KEY = os.environ.get("VERY_STREAM_KEY", None)
