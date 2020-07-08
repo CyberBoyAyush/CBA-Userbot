ff03c88fd497034dad7e09cd99b9ad4583304ed8
StarkGang <StarkGangZ@gmail.com>
2020-07-08 11:47:51 +0530

    Added Termux Module

--- telesetup.py
+++ telesetup.py
@@ -1,18 +1,25 @@
-#!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677
-# This Source Code Form is subject to the terms of the GNU
-# General Public License, v.3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
-
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+print ("")
+print ("")
+print(""" To Friday String Generator By StarkGang""")
+print("""Kindly Enter Your Details To Continue ! """)
 
-print("""Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
-
-with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
-    print(client.session.save())
+API_KEY = '1754367'
+API_HASH = "231b8cc6cca12ee51a85cf543321f476"
+while True:
+  try:
+   with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+      print("")
+      session = client.session.save()
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @FridayOT For Any Help !")
+      print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
+      print("Store it safe !!")
+  except:
+   print ("")
+   print ("Wrong phone number \n make sure its with correct country code. Example : +919961998999 ! Kindly Retry")
+   print ("")
+   continue
+  break
+    
+= 
