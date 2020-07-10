a6968328b1513a029f97a90b903e7c115d121b97
StarkGang <StarkGangZ@gmail.com>
2020-07-10 18:24:41 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -2,6 +2,7 @@
 #IMG CREDITS: @WhySooSerious
 import asyncio
 from telethon import events
+from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
