98a0dd72c6278cd56827ba99ae666ad9a593514c
StarkGang <StarkGangZ@gmail.com>
2020-07-10 17:58:44 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -3,23 +3,6 @@
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-@@ -8,24 +9,22 @@
-
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-FRIDAY_IS_ALIVE = ("**F.R.I.D.A.Y** IS AT YOUR SERVICE❗\n\n"
-                   "**I am Always Alive To Help You Boss**\n\n"
-                   "**♧︎︎︎Currently Status♧︎︎︎** : `No Issue Found`\n\n"
-                   "**♧︎︎︎Current Branch♧︎︎︎** : `master`\n\n"
-                   "**♧︎︎︎Python Version♧︎︎︎** : `3.8`\n\n"
-                   "**♧︎︎︎Friday OS♧︎︎︎** : `3.0 Snapdragon`\n\n"
-                   "**♧︎︎︎Current Sat♧︎︎︎** : `StarkGangSat-2.0`\n\n"
-                   f"**♧︎︎︎My Boss♧︎︎︎** : {DEFAULTUSER} \n\n"
-                   "**♧︎︎︎Heroku Database♧︎︎︎** : `No Known Error Found`\n\n"
-                   "**♧︎︎︎License♧︎︎︎** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-                   "♧︎︎︎Copyright♧︎︎︎ : By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
-                   " [⌨︎Deploy FridayUserbot⌨︎](https://telegra.ph/FRIDAY-06-15)") 
-
 
 PM_IMG = "https://telegra.ph/file/0adbdf47628be382a67c7.jpg"
 pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
