590d400955a932c7ebd93efdb31d4a2752911bcf
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-07-19 19:36:53 +0530

    [FIX] Music Downloader
    Replaced the Alternate Downloader with new one!!
    
    By @WhySooSerious

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -1,49 +1,28 @@
+# modified for FRIDAY by @WhySooSerious
+import datetime
 from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
-import io
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 import asyncio
-import time
 from userbot.utils import admin_cmd
-import glob
-import os
-try:
- import instantmusic , subprocess
-except:
- os.system("pip install instantmusic")
- 
 
 
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-@borg.on(admin_cmd(pattern="song ?(.*)"))
+@register(outgoing=True, pattern="^.smd(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 0.5
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
-    bruh(str(cmd))
-    l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("Trying To send This Song")
-    await borg.send_file(
-                event.chat_id,
-                loa,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
+    name = event.pattern_match.group(1)
+    chat = "@SpotifyMusicDownloaderBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Downloading Music \nIt may take some time\n   So Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              await bot.send_message(chat, name)
+              respond = await response
+          except YouBlockedUserError:
+              await event.reply("```Please unblock @SpotifyMusicDownloaderBot and try again```")
+              return
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
