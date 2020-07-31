b72fb094f73c6d1b728c720da40852ef7d0036eb
StarkGang <StarkGangZ@gmail.com>
2020-07-31 12:03:28 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -26,7 +26,8 @@ from telethon.tl.types import (
     InputStickerSetShortName,
     MessageMediaPhoto
 )
-from userbot.utils import admin_cmd, ALIVE_NAME
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
 
 FILLED_UP_DADDY = "Invalid pack selected."
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name"
