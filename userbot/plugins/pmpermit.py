928c4e55901f7128c032a4c53f591a06059d52c6
StarkGang <StarkGangZ@gmail.com>
2020-08-05 11:12:21 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -188,7 +188,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 return
             except:
                 return
- await event.client.send_file(event.chat_id, WARN_PIC, caption=USER_BOT_NO_WARN)
+      r =  await event.client.send_file(event.chat_id, WARN_PIC, caption=USER_BOT_NO_WARN)
         PM_WARNS[chat_id] += 1
         if chat_id in PREV_REPLY_MESSAGE:
             await PREV_REPLY_MESSAGE[chat_id].delete()
