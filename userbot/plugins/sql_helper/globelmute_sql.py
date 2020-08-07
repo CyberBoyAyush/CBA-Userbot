739bc2be392ca45cbcfd896584f48c1c2ae20844
StarkGang <StarkGangZ@gmail.com>
2020-08-07 11:04:22 +0530

    Sorry didn't notice

--- userbot/plugins/sql_helper/globelmute_sql.py
+++ userbot/plugins/sql_helper/globelmute_sql.py
@@ -1,7 +1,7 @@
 # From Javes 05 Userbot ( For Gmute Plugin ) For Test ( I know I have it already but Wait Let Me Do Test )
 
 try:
-    from userbot.modules.sql_helper import SESSION, BASE
+    from userbot.plugins.sql_helper import SESSION, BASE
 except ImportError:
     raise AttributeError
 
