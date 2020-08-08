0e3d553d849fd116d6de811c10ec9d6ee91740e3
StarkGang <StarkGangZ@gmail.com>
2020-08-08 22:40:13 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
+            reply_pop_up_alert = "Please get your own Userbot, Join @FridayOT To Know How To Make !"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -93,7 +93,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("🔥", x ,"🔥"),
+        "{} {}".format("🔥" , x , "🔥"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
