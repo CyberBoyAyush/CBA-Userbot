21d4f8e478a81e4a71641d2a3a14dca220eac0b7
fayash <69240111+fayash@users.noreply.github.com>
2020-08-11 12:29:42 -0700

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -22,7 +22,7 @@ async def _(event):
             "What The",
             "What The F",
             "What The F Brah",
-            "[What The F Brah](https://telegra.ph//file/f3b760e4a99340d331f9b.jpg)"
+            "[What The F Brah](https://telegra.ph/file/b54bbc1dce80356bbc892.png)"
         ]
 
     for i in animation_ttl:
