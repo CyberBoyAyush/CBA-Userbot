862756362867592d84a39b0dbdf8bf4b96b31813
fayash <69240111+fayash@users.noreply.github.com>
2020-08-11 07:57:06 -0700

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [FRIDAY](https://www.github.com/midhunkm1294-bit/FRIDAY)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [GULFY](https://www.github.com/midhunkm1294-bit/FRIDAY)>> ",
 
          force_document=True,
 
