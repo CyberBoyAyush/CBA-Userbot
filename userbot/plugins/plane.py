bcdad6b824a3b6b1d357711ab3a24182bc574a5b
fayash <69240111+fayash@users.noreply.github.com>
2020-08-11 15:41:28 -0700

    Update plane.py

--- userbot/plugins/plane.py
+++ userbot/plugins/plane.py
@@ -29,24 +29,4 @@ async def _(event):
     await asyncio.sleep(3)
     await event.delete()
 
-
     
-    @borg.on(events.NewMessage(pattern=r"\.hrt", outgoing=True))
-    async def _(event):
-    if event.fwd_from:
-        return
-        
-        
-    await event.edit("❤️----------")
-    await event.edit("-🧡---------")
-    await event.edit("--💛--------")
-    await event.edit("---💚-------")
-    await event.edit("----💙------")
-    await event.edit("-----💜-----")
-    await event.edit("------🖤----")
-    await event.edit("-------🤍---")
-    await event.edit("--------💗--") 
-    await event.edit("---------🤎-")
-    await event.edit("----------💔")
-    await asyncio.sleep(3)
-    await event.delete()
