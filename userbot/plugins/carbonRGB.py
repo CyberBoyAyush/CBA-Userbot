e75be8955e433646dd663dcb0d57b3faafeba978
fayash <69240111+fayash@users.noreply.github.com>
2020-08-11 07:58:33 -0700

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -100,7 +100,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [FRIDAY](https://github.com/midhunkm1294-bit/Friday)",
+         caption="Carbonised by [GULFY](https://github.com/midhunkm1294-bit/Friday)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
