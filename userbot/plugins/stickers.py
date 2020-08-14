ebc2f7b734740efdf5fcc9fa3ee4eb7dc50c9175
fayash <69240111+fayash@users.noreply.github.com>
2020-08-14 04:39:23 -0700

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -19,16 +19,7 @@ from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
 
 KANGING_STR = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "hehe me stel ur stikér\nhehe.",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal Your Sticker is stealing this sticker... ",
+    "🧞‍♀️🌪 im Stealing this Stiker For My Master 🧞‍♀️🌪...",
 ]
 
 
@@ -71,10 +62,10 @@ async def kang(args):
             is_anim = True
             photo = 1
         else:
-            await args.edit("`Unsupported File!`")
+            await args.edit("`Unsupported🧞‍♀️🌪File!`")
             return
     else:
-        await args.edit("`I can't kang that...`")
+        await args.edit("`I🧞‍♀️can't🌪kang🌪that🧞‍♀️...`")
         return
 
     if photo:
@@ -236,8 +227,8 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"Sticker kanged successfully!\
-            \nPack can be found [here](t.me/addstickers/{packname})",
+        await args.edit(f"🧞‍♀️now💫this✨is💫your's🧞‍♀️Master!\
+            \n [🌜Take🧞‍♀️it🌛](t.me/addstickers/{packname})",
                         parse_mode='md')
 
 
