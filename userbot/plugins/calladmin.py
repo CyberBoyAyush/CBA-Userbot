517e9861e78d92088477c405e2d9833869823ed9
StarkGang <StarkGangZ@gmail.com>
2020-08-15 22:33:53 +0530

    Update calladmin.py

--- userbot/plugins/calladmin.py
+++ userbot/plugins/calladmin.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="admins"))
+@borg.on(admin_cmd(pattern="admins", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
