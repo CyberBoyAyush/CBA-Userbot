4724f5aeea8f58fd3e390d4808955859278707f9
fayash <69240111+fayash@users.noreply.github.com>
2020-08-16 12:59:06 -0700

    Update stickerchat.py

--- userbot/plugins/stickerchat.py
+++ userbot/plugins/stickerchat.py
@@ -57,7 +57,7 @@ if 1 == 1:
                                           "default_username_color": "#b48bf2"})
     client = borg
 
-    @borg.on(admin_cmd(pattern="stchat(.*)"))
+    @borg.on(admin_cmd(pattern="stca (.*)"))
     async def quotecmd(message):  # noqa: C901
         """Quote a message.
         Usage: .quote [template]
