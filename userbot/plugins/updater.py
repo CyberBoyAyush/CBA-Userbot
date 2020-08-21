504d0ba9dfd4932c5054ada7a3fa61c4f2f27ab7
Ayush Sharma <69210117+CyberBoyAyush@users.noreply.github.com>
2020-08-21 21:10:23 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -15,10 +15,10 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/StarkGang/FridayUserbot"
-BOT_IS_UP_TO_DATE = "Friday userbot is up-to-date!"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/CyberBoyAyush/CBA-Userbot"
+BOT_IS_UP_TO_DATE = "CBA Userbot is up-to-date!"
 NEW_BOT_UP_DATE_FOUND = (
-    "**Friday Update Found For** {branch_name}\n"
+    "**CBA Userbot Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
     "Pulling Updates !!"
 )
@@ -72,7 +72,7 @@ async def updater(message):
     )
 
     if not changelog:
-        await message.edit("**Updating Userbot** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `Pulling Updates` \n**Thank You For Using Friday !**")
+        await message.edit("**Updating Userbot** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `Pulling Updates` \n**Thank You For Using CBA Userbot !**")
         await asyncio.sleep(5)
  
     message_one = NEW_BOT_UP_DATE_FOUND.format(
