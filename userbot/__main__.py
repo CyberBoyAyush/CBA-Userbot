71898e22c682edb71a8e01b6a9988c8812ebfe3a
Ayush Sharma <69210117+CyberBoyAyush@users.noreply.github.com>
2020-08-24 13:00:26 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Friday Has Been Enabled ! Try .ping")
+print("CBA Userbot Is Working Say Thnx To @CyberBoyAyush! Try .ping")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
