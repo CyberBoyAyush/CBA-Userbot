c5890d8f5eeb96268338ecedef6c8b6dba490394
Ayush Sharma <69210117+CyberBoyAyush@users.noreply.github.com>
2020-08-25 12:16:40 +0530

    Update eval.py

--- userbot/plugins/eval.py
+++ userbot/plugins/eval.py
@@ -1,21 +1,91 @@
-"""Evaluate Python Code inside Telegram
-Syntax: .eval PythonCode"""
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from telethon import events, errors, functions, types
-import inspect
-import traceback
-import asyncio
-import sys
+"""Execute GNU/Linux commands inside Telegram
+Syntax: .exec Code"""
 import io
-from uniborg.util import admin_cmd
+import sys
+import time
+import inspect
+import asyncio
+import traceback
+import subprocess
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd,sudo_cmd
+from telethon import events, errors, functions, types
+from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 
-
-@borg.on(admin_cmd("eval"))
+@borg.on(admin_cmd(pattern="bash ?(.*)"))
 async def _(event):
-    if event.fwd_from:
+    if event.fwd_from or event.via_bot_id:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    OUTPUT = f"`{stdout.decode()}`"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "bash.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit(OUTPUT)
+        
+@borg.on(admin_cmd(pattern="exec ?(.*)"))
+async def _(event):
+    if event.fwd_from or event.via_bot_id:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    e = stderr.decode()
+    if not e:
+        e = "No Error"
+    o = stdout.decode()
+    if not o:
+        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
+    else:
+        _o = o.split("\n")
+        o = "`\n".join(_o)
+    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit(OUTPUT)
+
+@borg.on(admin_cmd(pattern="eval"))
+async def _(event):
+    if event.fwd_from or event.via_bot_id:
         return
     await event.edit("Processing ...")
     cmd = event.text.split(" ", maxsplit=1)[1]
@@ -66,10 +136,49 @@ async def _(event):
     else:
         await event.edit(final_output)
 
-
 async def aexec(code, event):
-    exec(
-        f'async def __aexec(event): ' +
+    exec(f'async def __aexec(event): ' +
         ''.join(f'\n {l}' for l in code.split('\n'))
-    )
+        )
     return await locals()['__aexec'](event)
+
+@borg.on(sudo_cmd(pattern="bash ?(.*)" ,allow_sudo=True))
+async def _(event):
+    if event.fwd_from or event.via_bot_id:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    OUTPUT = f"`{stdout.decode()}`"
+    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "bash.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.reply(OUTPUT)
+        
+CMD_HELP.update({
+    "evaluators":".eval <expr>`:\
+     \n**USAGE : **Execute Python script.\
+     \n\n.exec <command>`:\
+     \n**USAGE : **Execute a bash command on catuserbot server and shows details.\
+     \n\n.bash <command>`:\
+     \n**USAGE : **Execute a bash command on catuserbot server and  easy to copy output\
+     "
+})
