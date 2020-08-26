d1330d3304084d85548ac0972bfde3236b0296cb
InsaneAshmit <70191984+InsaneAshmit@users.noreply.github.com>
2020-08-26 09:32:50 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,5 +1,3 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-#IMG CREDITS: @WhySooSerious
 import asyncio
 from telethon import events
 from uniborg.util import admin_cmd
@@ -7,15 +5,14 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/fc3aef09eb9b82d244f97.jpg"
-pm_caption = "`CBA Userbot IS:` **ONLINE**\n\n"
+pm_caption = "`R.A.G.N.A.R Userbot IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
 pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**CBA Userbot OS** : `3.14`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Made By 😎** : @CyberBoyAyush\n\n"
-pm_caption += "Deploy Your Own : [Repo](https://github.com/CyberBoyAyush/CBA-Userbot)\n"
+pm_caption += "**R.A.G.N.A.R OS** : `3.14`\n"
+pm_caption += "**Made By 😎** : @Insane_Ashmit\n\n"
+pm_caption += "Want to Deploy Your Own : DM my master\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
